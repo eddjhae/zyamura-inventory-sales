a63857fe3003cad21dd123e77b97689c4b8a9f1f
Edd Jhae Bausa <40552193+eddjhae@users.noreply.github.com>
2022-09-03 14:46:46 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sriracha: ["Sriracha", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+      colors: {
+        surface: "#e0e4d9",
+        "primary-light": "#D9D7C4",
+        "primary-dark": "#667061",
+      },
+      // gridTemplateColumns: {
+      //   products: "50px repeat(7, minmax(0, 5))",
+      // },
+    },
+  },
+};
